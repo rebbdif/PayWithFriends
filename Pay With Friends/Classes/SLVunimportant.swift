b5c5af3d63a98b3609b3c6a48168fd074c5c6787
Julia Serebryanaya <juliaserebr@gmail.com>
2018-03-15 01:26:42 +0300

    renamed account

---
+++ Pay With Friends/Classes/SLVunimportant.swift
@@ -0,0 +1,13 @@
+//
+//  SLVunimportant.swift
+//  Pay With Friends
+//
+//  Created by 1 on 15.03.18.
+//  Copyright © 2018 Леонид Серебряный. All rights reserved.
+//
+
+import UIKit
+
+class SLVunimportant: NSObject {
+
+}
