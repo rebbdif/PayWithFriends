544679a2df86a2c7fd8b22655b836a47173bb81f
Julia Serebryanaya <juliaserebr@gmail.com>
2018-03-15 15:29:27 +0300

    добавила SLVItemDetailed

--- Pay With Friends/Classes/SLVImageRecognition.swift
+++ Pay With Friends/Classes/SLVImageRecognition.swift
@@ -23,6 +23,36 @@ class SLVImageRecognition: NSObject {
 		var result = [SLVItem]()
 		// вот сюда приделывай вызовы CoreML и тд.
 		// приделаю
+		
 		return result
 	}
+}
+
+// НАДО НЕ ЗАБЫТЬ ПРО ЧАЕВЫЕ
+// буду сюда считывать
+public class SLVItemDetailed: NSObject {
+	var name: String
+	var number: Int //количество. потом надо изменить массив так, чтобы везде стало единицей (повторить значения, где оно не 1)
+	var price: Double
+	var info: String
+	var person: [String]
+	
+	init(name: String, number: Int, price: Double, info: String, person: [String]) {
+		self.name = name
+		self.number = number
+		self.price = price
+		self.info = info
+		self.person = person
+	}
+	public override func isEqual(_ object: Any?) -> Bool {
+		guard let second = object as? SLVItemDetailed else { return false }
+		
+		return second.name == self.name &&
+		second.number == self.number &&
+		second.price == self.price &&
+		second.info == self.info &&
+		second.person == self.person
+		
+	}
+	
 }
